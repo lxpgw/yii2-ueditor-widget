2e9e837212046ec1a4436645ae6b1a237e58e046
xjflyttp <xjflyttp@gmail.com>
2014-05-23 14:50:08 +0800

    1450

---
+++ UeditorAssets.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Lepture Markdown Editor assets class file
+ *
+ * @author Evgeniy Kuzminov
+ * @license MIT License
+ * http://stdout.in
+ */
+namespace xj;
+
+use yii\web\AssetBundle;
+use yii;
+
+class UeditorAssets extends AssetBundle
+{
+	public $sourcePath = '@xj-ueditor/assets';
+	public $basePath = '@webroot/assets';
+	public $js = [
+		'js/ueditor.config.js',
+		'js/ueditor.all.js',
+	];
+	public $css = [
+		//'css/editor.css',
+	];
+	public $depends = [
+		//'yii\web\JqueryAsset',
+	];
+
+	public function init() {
+		Yii::setAlias('@xj-ueditor', __DIR__);
+		return parent::init();
+	}
+}
